18fd471d276e80c6b24227ff5b1931f387d1c4fb
CHAMODKAVINDA <93853983+CHAMODKAVINDA@users.noreply.github.com>
2023-10-25 13:03:04 +0530

    save customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,9 +1,20 @@
-public class Customer {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-        private Long id ;
-        private String name;
-        private String address;
-        private double salary;
+@Entity
+@Table(name="customer")
+public class Customer {
+    @Id
+    @Column(name="customer_id")
+    private Long id ;
+    @Column(name="customer_name")
+    private String name;
+    @Column(name="customer_address")
+    private String address;
+    @Column(name="customer_salary")
+    private double salary;
 
     public Customer(Long id, String name, String address, double salary) {
         this.id = id;
